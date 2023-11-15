485a50b71d9fb5c0e0b4e53c3abc981ce076ffb0
Lasse Wolpmann <lassewolpmann@outlook.de>
2023-11-15 15:11:36 +0200

    Initial Commit

---
+++ f1-countdown-ios/f1_countdown_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  f1_countdown_iosApp.swift
+//  f1-countdown-ios
+//
+//  Created by Lasse Wolpmann on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct f1_countdown_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
