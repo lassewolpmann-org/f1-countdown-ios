8dab5deff6bfbc519570481ba9150c9717b2dcad
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-04-20 12:14:31 +0300

    Changed Session name from Sprint Shootout to Sprint Qualifying

--- Helpers/SessionName.swift
+++ Helpers/SessionName.swift
@@ -9,50 +9,50 @@ import Foundation
 
 func parseSessionName(sessionName: String?) -> String {    
     switch (sessionName) {
-    case "fp1":
-        return "Free Practice 1"
-    case "fp2":
-        return "Free Practice 2"
-    case "fp3":
-        return "Free Practice 3"
-    case "practice":
-        return "Practice"
-    case "qualifying":
-        return "Qualifying"
-    case "sprintQualifying":
-        return "Sprint Shootout"
-    case "sprint":
-        return "Sprint"
-    case "gp":
-        return "Race"
-    case "feature":
-        return "Feature"
-    default:
-        return "Undefined Session"
+        case "fp1":
+            return "Free Practice 1"
+        case "fp2":
+            return "Free Practice 2"
+        case "fp3":
+            return "Free Practice 3"
+        case "practice":
+            return "Practice"
+        case "qualifying":
+            return "Qualifying"
+        case "sprintQualifying":
+            return "Sprint Qualifying"
+        case "sprint":
+            return "Sprint"
+        case "gp":
+            return "Race"
+        case "feature":
+            return "Feature"
+        default:
+            return "Undefined Session"
     }
 }
 
 func parseShortSessionName(sessionName: String?) -> String {
     switch (sessionName) {
-    case "fp1":
-        return "FP1"
-    case "fp2":
-        return "FP2"
-    case "fp3":
-        return "FP3"
-    case "practice":
-        return "FP"
-    case "qualifying":
-        return "Q"
-    case "sprintQualifying":
-        return "SQ"
-    case "sprint":
-        return "Sprint"
-    case "gp":
-        return "Race"
-    case "feature":
-        return "Feature"
-    default:
-        return "?"
+        case "fp1":
+            return "FP1"
+        case "fp2":
+            return "FP2"
+        case "fp3":
+            return "FP3"
+        case "practice":
+            return "P"
+        case "qualifying":
+            return "Q"
+        case "sprintQualifying":
+            return "SQ"
+        case "sprint":
+            return "Sprint"
+        case "gp":
+            return "Race"
+        case "feature":
+            return "Feature"
+        default:
+            return "?"
     }
 }
